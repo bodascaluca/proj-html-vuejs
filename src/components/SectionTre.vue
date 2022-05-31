dffa4ec9587f6f8fce985a614f605af416519db6
bodascaluca <bodasca.luca@gmail.com>
2022-05-31 22:10:24 +0200

    Add sectionTre

--- src/components/SectionTre.vue
+++ src/components/SectionTre.vue
@@ -1,36 +1,47 @@
 <template>
      <div class="ms_container sectionTre d-flex justify-content-between">
 
-         <div class="border text-center col-3 aaa">
-            <div class="col-uno-our text-uppercase">our products</div>
-            <h4 class="col-uno-tx mt-3 mb-5">All our delectable pastries are backed fresh in our Kitchen very morning, and are made with all-natural. all organic ingredients.</h4>
-            <button class="btn-prymary btn-tx ms_btn-style-primary" ><a href="#">Start Shopping</a></button>
+         <div class="text-center col-3 aaa">
+             <h4 class="col-uno-tx  mt-5 mb-3">Find a freshly backed product perfect for you</h4>
+            <div class="col-uno-our mt-1">Integer a nibh vitae ex porttitor rutrum et ut velit. Etiam ac felis at leo feugiat placerat. Sed  ac nulla id orci tempor convallis sed.</div>
+            <button class="btn-prymary btn-tx ms_btn-style-primary mt-3"><a href="#">Shop All Products</a></button>
          </div>
 
-         <div class="border imageUno col-2 aaa"></div>
-
-         <div class="border imageDuo col-2 aaa"></div>
-
-         <div class="border imageTre col-2 aaa"></div>
-
-         <div class="border imageQuattro col-2 aaa"></div>
-
-
-        <!-- <div class="col-5 me-5 aaa">
-            <div class="col-uno-our text-uppercase">our products</div>
-            <h4 class="col-uno-tx mt-3 mb-5">All our delectable pastries are backed fresh in our Kitchen very morning, and are made with all-natural. all organic ingredients.</h4>
-            <button class="btn-prymary btn-tx ms_btn-style-primary" ><a href="#">Start Shopping</a></button>
-        </div> -->
-
-        <!-- <div class="select-container-left col-5 aaa">
-            <div class="select select-left"> <i class="fas fa-angle-left"></i> </div>
-        </div>
-
-        <div class="select-container-right col-5 aaa">
-            <div class="select select-right"><i class="fas fa-angle-right"></i> </div>
-        </div> -->
-
+         <div class="col-2 aaa">
+            <div class="select-container-left  imageUno">
+                 <div class="select select-left"> <i class="fas fa-angle-left"></i> </div>
+            </div>
+            <div class="text-center mt-3">
+                    <b class="img-name">Choco Chip Cookies</b>
+                    <h6 class="img-price mt-2">$19.00 - $39.00</h6>
+            </div>
+         </div>
         
+         <div class="col-2 aaa">
+             <div class="img imageDuo"></div>
+              <div class="text-center mt-3">
+                    <b class="img-name">Strawberry Jam Cookies</b>
+                    <h6 class="img-price mt-2">$24.00 - $62.00</h6>
+            </div>
+         </div>
+
+         <div class="col-2 aaa">
+             <div class="img imageTre"></div>
+              <div class="text-center mt-3">
+                    <b class="img-name">Strawberry Donut</b>
+                    <h6 class="img-price mt-2">$24.00 - $42.00</h6>
+            </div>
+         </div>
+
+         <div class="col-2 aaa">
+             <div class="select-container-right  imageQuattro">
+                 <div class="select select-right"><i class="fas fa-angle-right"></i> </div>
+             </div> 
+              <div class="text-center mt-3">
+                    <b class="img-name">Perfect Macarons</b>
+                    <h6 class="img-price mt-2">$18.00 - $52.00</h6>
+            </div>
+         </div>
     </div>
 </template>
 
@@ -45,7 +56,6 @@ export default {
 @import "../style/btn.scss";
 
 .sectionTre{
-    border:1px solid orange;
 
      .aaa{
         margin: 4rem 0; 
@@ -53,11 +63,13 @@ export default {
         
             .col-uno-our{
                 color:$color-grey-text;
-                font-size: .7rem;
+                font-size: .9rem;
             }
 
             .col-uno-tx{
                 color:$brand-primary-color;
+                padding-top:3rem;
+
             }
         } 
 
@@ -68,7 +80,7 @@ export default {
             }
 
         .select-container-left{
-            height: 400px;
+            height: 300px;
             background-image: url("../assets/image/choco-chip-cookies-400x510.jpg");
             background-position: center;
             background-size:cover;
@@ -77,13 +89,13 @@ export default {
             
               .select-left{
                     position: absolute;
-                    top: 43%;
+                    top: 40%;
                     left: 0;
                 }
         }
 
          .select-container-right{
-             height: 400px;
+             height: 300px;
             background-image: url("../assets/image/strawberry-jam-cookies-400x510.jpg");
             background-position: center;
             background-size:cover;
@@ -92,36 +104,39 @@ export default {
             
              .select-right{
                     position: absolute;  
-                    top: 43%;
+                    top: 40%;
                     right: 0;
                 }
         }
-
-        .imageUno{
-            background-image: url("../assets/image/choco-chip-cookies.jpg");
-             background-position: center;
+        .img{
+            height:300px;
+            background-position: center;
             background-size:cover;
             background-repeat:no-repeat;
+        }
+        .imageUno{
+            background-image: url("../assets/image/choco-chip-cookies.jpg");
         }
 
           .imageDuo{
             background-image: url("../assets/image/strawberry-jam-cookies.jpg");
-             background-position: center;
-            background-size:cover;
-            background-repeat:no-repeat;
         }
           .imageTre{
             background-image: url("../assets/image/strawberry-donut.jpg");
-             background-position: center;
-            background-size:cover;
-            background-repeat:no-repeat;
         }
 
           .imageQuattro{
             background-image: url("../assets/image/perfect-macarons.jpg");
-             background-position: center;
-            background-size:cover;
-            background-repeat:no-repeat;
+        }
+
+        .img-name{
+                color:$brand-primary-color;
+                font-size:1.2rem;
+        }
+
+        .img-price{
+            color:$brand-primary-color;  
+            font-size:.8rem;    
         }
 }
 </style>
